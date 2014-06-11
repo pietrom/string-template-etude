26b6d1820fa71c242f41ed6b6dd3381e66a6e2f4
Pietro Martinelli <amicofragile@gmail.com>
2014-06-11 16:36:46 +0200

    Inline template group sample

--- src/test/java/org/amicofragile/etude/st/InlineTemplateTest.java
+++ src/test/java/org/amicofragile/etude/st/InlineTemplateTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupString;
 
 public class InlineTemplateTest {
 	@Test
@@ -12,5 +14,23 @@ public class InlineTemplateTest {
 		final ST st = new ST(template);
 		st.add("name", "World");
 		assertEquals("Hello, World!", st.render());
+	}
+	
+	@Test
+	public void groupSample() throws Exception {
+		final String groupString = 
+				"lc(name) ::= <<\n" + 
+						"hello, <name>!\n" +
+						">>\n" + 
+				"uc(name) ::= <<\n" +
+						"HELLO, <name>!\n" + 
+				">>\n";
+		final STGroup group = new STGroupString(groupString);
+		final ST lc = group.getInstanceOf("lc");
+		lc.add("name", "World");
+		assertEquals("hello, World!", lc.render());
+		final ST uc = group.getInstanceOf("uc");
+		uc.add("name", "World");
+		assertEquals("HELLO, World!", uc.render());
 	}
 }
