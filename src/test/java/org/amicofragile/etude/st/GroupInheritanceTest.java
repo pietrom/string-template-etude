fea99ab2029b672c5e4b9e3a747d7a98cf91d740
Pietro Martinelli <amicofragile@gmail.com>
2014-06-16 16:26:29 +0200

    Simple inheritance sample: template inheritance through import

---
+++ src/test/java/org/amicofragile/etude/st/GroupInheritanceTest.java
@@ -0,0 +1,18 @@
+package org.amicofragile.etude.st;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+
+public class GroupInheritanceTest {
+	@Test
+	public void inheritanceThroughImport() throws Exception {
+		final STGroup group = new STGroupFile(getClass().getResource("/inheritance/child.stg"), "UTF-8", '<', '>');
+		final ST st = group.getInstanceOf("hello");
+		st.add("name", "World");
+		assertEquals("Hello, World!", st.render());
+	}
+}
