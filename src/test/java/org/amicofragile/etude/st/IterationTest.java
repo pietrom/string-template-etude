4ac872ccbfa17651bfe97297b99f02e5d0fa880b
Pietro Martinelli <amicofragile@gmail.com>
2014-06-16 14:37:29 +0200

    'iterate and apply anonymous template' sample

--- src/test/java/org/amicofragile/etude/st/IterationTest.java
+++ src/test/java/org/amicofragile/etude/st/IterationTest.java
@@ -1,6 +1,9 @@
 package org.amicofragile.etude.st;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Test;
 import org.stringtemplate.v4.ST;
@@ -11,6 +14,18 @@ public class IterationTest {
 		final String template = "Hello, <persons; separator=\", \">!";
 		final ST st = new ST(template);
 		st.add("persons", new String[] {"Qui", "Quo", "Qua"});
+		assertEquals("Hello, Qui, Quo, Qua!", st.render());
+	}
+	
+	@Test
+	public void iterateAndApplyAnonymousTemplate() throws Exception {
+		final String template = "Hello, <persons:{p | <p.firstname>}; separator=\", \">!";
+		final ST st = new ST(template);
+		final List<Person> persons = new LinkedList<Person>();
+		persons.add(new Person("Qui", "Duck"));
+		persons.add(new Person("Quo", "Duck"));
+		persons.add(new Person("Qua", "Duck"));
+		st.add("persons", persons);
 		assertEquals("Hello, Qui, Quo, Qua!", st.render());
 	}
 }
